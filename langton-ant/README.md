25ef12847aa803179aa15de85fe6be23dec41941
thomaswoehlke <thomas.woehlkegmail.com>
2019-12-19 11:49:29 +0100

    initial setup

---
+++ langton-ant/README.md
@@ -0,0 +1,5 @@
+# Langton's ant
+* https://en.wikipedia.org/wiki/Langton%27s_ant
+* https://de.wikipedia.org/wiki/Ameise_(Turingmaschine)
+
+
---
+++ simulated-evolution/src/test/java/org/woehlke/simulation/wator/AppTest.java
@@ -0,0 +1,18 @@
+package org.woehlke.simulation.wator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple AppMainDesktop.
+ */
+public class AppTest {
+
+  /**
+   * When I am grown up, I will be a cool Uni Test.
+   */
+  @Test
+  public void testApp() {
+    Assert.assertTrue(true);
+  }
+}
