5bfa384c46d7a8327f621313e0b06a62cdd78a4f
Thomas Woehlke <thomas.woehlke@gmail.com>
2019-12-24 11:45:26 +0100

    added Swing tutorial Subproject

---
+++ swingtutorial/src/main/java/org/woehlke/swingtutorial/TutorialApplication.java
@@ -0,0 +1,15 @@
+package org.woehlke.swingtutorial;
+
+public class TutorialApplication {
+
+
+    /**
+     * Starting the Desktop Application.
+     *
+     * @param args CLI Parameter.
+     */
+    public static void main(String[] args) {
+        TutorialApplication application = new TutorialApplication();
+        System.out.println(": Started the Desktop Application");
+    }
+}
---
+++ swingtutorial/src/test/java/org/woehlke/swingtutorial/TutorialApplicationTest.java
@@ -0,0 +1,4 @@
+package org.woehlke.swingtutorial;
+
+public class TutorialApplicationTest {
+}
