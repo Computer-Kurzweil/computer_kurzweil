07dfb4fc2efb4532aeabda9275f827d05ee77747
Thomas Woehlke <thomas.woehlke@gmail.com>
2019-12-24 17:12:43 +0100

    updated gui

---
+++ mandelbrot/src/main/java/org/woehlke/simulation/mandelbrot/model/fractal/impl/GaussianNumberPlaneImpl.java
@@ -0,0 +1,15 @@
+package org.woehlke.simulation.mandelbrot.model.fractal.impl;
+
+import org.woehlke.simulation.mandelbrot.control.ObjectRegistryAndEventDispatcher;
+
+public class GaussianNumberPlaneImpl {
+
+    public GaussianNumberPlaneImpl(ObjectRegistryAndEventDispatcher model) {
+
+    }
+
+
+
+}
+
+
