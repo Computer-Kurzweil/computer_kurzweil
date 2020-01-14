0afb81cfb3f4f195dc9c9846e5a7cee4156b8be0
thomaswoehlke <thomas.woehlkegmail.com>
2020-01-14 15:57:43 +0100

    switch from one app to allinone app

--- allinone/src/main/java/org/woehlke/simulation/allinone/view/ComputerKurzweilApplicationFrame.java
+++ allinone/src/main/java/org/woehlke/simulation/allinone/view/ComputerKurzweilApplicationFrame.java
@@ -1,7 +1,6 @@
 package org.woehlke.simulation.allinone.view;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
