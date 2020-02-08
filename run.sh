2b01a418cef9ce27ce512e48cab5d9e2648a4e69
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-02-08 23:28:06 +0100

    remove springboot

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 function bootRunAllinone() {
     ./gradlew clean allinone:jar
-    ./gradlew allinone:run
+    java -jar allinone/build/libs/allinone-0.0.1-SNAPSHOT.jar
+    #./gradlew allinone:run
 }
 
 function bootRunDefault() {
