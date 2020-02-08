10920410c4ba810de337324a8a0aed1a3f63bf23
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-02-08 23:02:37 +0100

    asdf

--- run.sh
+++ run.sh
@@ -4,17 +4,9 @@ function bootRunAllinone() {
     ./gradlew clean allinone:assemble
 }
 
-function bootRunHerokuLocal() {
-    ./gradlew composeUp
-    ./gradlew clean assemble
-    heroku local web
-    ./gradlew composeDown
-}
-
 function bootRunDefault() {
     ./gradlew clean allinone:run
 }
 
 bootRunAllinone
-#bootRunHeroku
 #bootRunDefault
