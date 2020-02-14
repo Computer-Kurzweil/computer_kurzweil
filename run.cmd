39b0548808bf573c6a443a3eaab9af3909f37737
thomaswoehlke <thomas.woehlke@gmail.com>
2020-02-14 19:29:30 +0100

    bugfixing

--- run.cmd
+++ run.cmd
@@ -1,3 +1,2 @@
-gradlew clean allinone:run
-rem gradlew clean allinone:clean allinone:shadowJar
-rem java -cp allinone/build/libs/allinone-all.jar org.woehlke.computer.kurzweil.ComputerKurzweilApplication
+gradlew clean allinone:clean allinone:shadowJar --warning-mode all
+java -cp allinone/build/libs/allinone-all.jar org.woehlke.computer.kurzweil.ComputerKurzweilApplication
