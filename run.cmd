f978ea1cbad6e7f3c919e343ff00281a906318d5
thomaswoehlke <thomas.woehlke@gmail.com>
2020-02-14 18:22:25 +0100

    bugfixing

--- run.cmd
+++ run.cmd
@@ -1,1 +1,3 @@
-gradlew clean mandelbrot:run
+gradlew clean allinone:run
+rem gradlew clean allinone:clean allinone:shadowJar
+rem java -cp allinone/build/libs/allinone-all.jar org.woehlke.computer.kurzweil.ComputerKurzweilApplication
