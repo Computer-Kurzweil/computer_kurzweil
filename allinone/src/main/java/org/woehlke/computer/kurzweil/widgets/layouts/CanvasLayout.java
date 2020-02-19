47d9167f2fdb6c0ef6e03c718d4ab0d94542bdec
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-02-19 23:34:58 +0100

    updated

--- allinone/src/main/java/org/woehlke/computer/kurzweil/widgets/layouts/CanvasLayout.java
+++ allinone/src/main/java/org/woehlke/computer/kurzweil/widgets/layouts/CanvasLayout.java
@@ -3,16 +3,15 @@ package org.woehlke.computer.kurzweil.widgets.layouts;
 import javax.swing.*;
 import java.awt.*;
 
-public class CanvasLayout extends BoxLayoutVertical {
+public class CanvasLayout extends CenterFlowLayout {
 
     /**
      * Creates a layout manager that will lay out components along the
      * given axis.
      *
-     * @param target the container that needs to be laid out
      * @throws AWTError if the value of {@code axis} is invalid
      */
     public CanvasLayout(JComponent target) {
-        super(target);
+        //super(target);
     }
 }
