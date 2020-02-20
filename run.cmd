dbc07192dc0e6080aba7932241a08c0154c250cc
thomaswoehlke <thomas.woehlke@gmail.com>
2020-02-20 15:55:46 +0100

    work in progress

--- run.cmd
+++ run.cmd
@@ -1,5 +1,4 @@
-cmd /c gradlew -i clean allinone:clean allinone:test
+rem cmd /c gradlew -i clean allinone:clean allinone:test
 
-rem cmd /c gradlew clean allinone:clean allinone:shadowJar
-
-rem cmd /c java -cp allinone/build/libs/allinone-all.jar org.woehlke.computer.kurzweil.ComputerKurzweilApplication > log.txt
+cmd /c gradlew clean allinone:clean allinone:shadowJar
+cmd /c java -cp allinone/build/libs/allinone-all.jar org.woehlke.computer.kurzweil.ComputerKurzweilApplication > log.txt
