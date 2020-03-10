e4a2f41b1a1f03deaa119a5185e4fcac652c6c3d
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-03-10 20:08:29 +0100

    devops MAKE MULTIMODULE PROJECT

---
+++ insourcing_cyclic_cellular_automaton/src/test/java/org/woehlke/simulation/cyclic/cellular/automaton/CyclicCellularAutomatonTest.java
@@ -0,0 +1,19 @@
+package org.woehlke.simulation.cyclic.cellular.automaton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple CyclicCellularAutomatonRunner.
+ */
+public class CyclicCellularAutomatonTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue(true);
+    }
+}
--- run.sh
+++ run.sh
@@ -12,7 +12,8 @@ function runDefault() {
 }
 
 function runTest() {
-    ./gradlew -i  --warning-mode all computer_kurzweil_app:clean computer_kurzweil_app:build computer_kurzweil_app:test computer_kurzweil_app:check computer_kurzweil_app:runShadow
+    #./gradlew -i  --warning-mode all computer_kurzweil_app:clean computer_kurzweil_app:build computer_kurzweil_app:test computer_kurzweil_app:check computer_kurzweil_app:runShadow
+    ./gradlew -i  --warning-mode all insourcing_cyclic_cellular_automaton:clean insourcing_cyclic_cellular_automaton:build insourcing_cyclic_cellular_automaton:test insourcing_cyclic_cellular_automaton:check insourcing_cyclic_cellular_automaton:runShadow
 }
 
 runTest
