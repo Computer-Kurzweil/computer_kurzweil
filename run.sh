3c8d4220106e7cbed0ce2b32335ae2aba7388aeb
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-03-10 22:23:24 +0100

    devops MAKE MULTIMODULE PROJECT

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 function runJar() {
-    ./gradlew -i clean
-    ./gradlew -i shadowJar
-    java -cp build/libs/computer_kurzweil-all.jar org.woehlke.computer.kurzweil.ComputerKurzweilApplication
+    ./gradlew -i computer_kurzweil_app:clean
+    ./gradlew -i computer_kurzweil_app:shadowJar
+    java -cp computer_kurzweil_app/build/libs/computer_kurzweil-all.jar org.woehlke.computer.kurzweil.ComputerKurzweilApplication
 }
 
 function runDefault() {
-    ./gradlew -i clean
-    ./gradlew -i --warning-mode all runShadow
+    ./gradlew -i computer_kurzweil_app:clean
+    ./gradlew -i --warning-mode all computer_kurzweil_app:runShadow
 }
 
 function runSubmodule() {
@@ -48,19 +48,18 @@ function runAlllTests() {
         echo $i
         buildSubmodule $i
     done
-    #./gradlew -i --warning-mode all computer_kurzweil_app:clean computer_kurzweil_app:build computer_kurzweil_app:test computer_kurzweil_app:check computer_kurzweil_app:runShadow
-    #./gradlew -i --warning-mode all insourcing_cyclic_cellular_automaton:clean insourcing_cyclic_cellular_automaton:build insourcing_cyclic_cellular_automaton:test insourcing_cyclic_cellular_automaton:check insourcing_cyclic_cellular_automaton:runShadow
-    #./gradlew -i --warning-mode all insourcing_mandelbrot:clean insourcing_mandelbrot:build insourcing_mandelbrot:test insourcing_mandelbrot:check insourcing_mandelbrot:runShadow
-    #./gradlew -i --warning-mode all insourcing_simulated_evolution:clean insourcing_simulated_evolution:build insourcing_simulated_evolution:test insourcing_simulated_evolution:check insourcing_simulated_evolution:runShadow
-    #./gradlew -i --warning-mode all insourcing_diffusion_limited_aggregation:clean insourcing_diffusion_limited_aggregation:build insourcing_diffusion_limited_aggregation:test insourcing_diffusion_limited_aggregation:check insourcing_diffusion_limited_aggregation:runShadow
 }
 
-#runJar
-#runDefault
 
-#runAlllTests
-#runSubmoduleCca
-#runSubmoduleDla
-runSubmoduleMandelbrot
-#runSubmoduleSimulatedEvolution
-#runSubmoduleMain
+
+
+function main() {
+    #runJar
+    #runDefault
+    #runAlllTests
+    #runSubmoduleCca
+    #runSubmoduleDla
+    runSubmoduleMandelbrot
+    #runSubmoduleSimulatedEvolution
+    #runSubmoduleMain
+}
