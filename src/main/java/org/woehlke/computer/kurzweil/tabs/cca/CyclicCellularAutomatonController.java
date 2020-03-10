6fbf97b197c67ee3f64249bfa23e41230039523d
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-03-10 19:02:40 +0100

    aa

--- src/main/java/org/woehlke/computer/kurzweil/tabs/cca/CyclicCellularAutomatonController.java
+++ src/main/java/org/woehlke/computer/kurzweil/tabs/cca/CyclicCellularAutomatonController.java
@@ -17,6 +17,13 @@ import org.woehlke.computer.kurzweil.commons.tabs.TabController;
  */
 @Log4j2
 @Getter
+/**
+ * https://docs.oracle.com/en/java/javase/13/docs/api/java.base/java/util/concurrent/ThreadPoolExecutor.html
+ * https://docs.oracle.com/en/java/javase/13/docs/api/java.base/java/util/concurrent/Executor.html
+ * TODO: https://github.com/Computer-Kurzweil/computer_kurzweil/issues/18
+ * TODO: https://github.com/Computer-Kurzweil/computer_kurzweil/issues/19
+ * http://openbook.rheinwerk-verlag.de/javainsel9/javainsel_14_004.htm
+ */
 public class CyclicCellularAutomatonController extends Thread
         implements TabController, CyclicCellularAutomaton {
 
@@ -46,9 +53,8 @@ public class CyclicCellularAutomatonController extends Thread
             }
             synchronized (this.tabCtx) {
                 //log.info("running");
-                this.tabCtx.getTabModel().step();
-                this.tabCtx.getCanvas().update();
-                this.tabCtx.getTab().repaint();
+                this.tabCtx.getTabModel().exec();
+                this.tabCtx.exec();
             }
             try { super.sleep( this.threadSleepTime ); }
             catch (InterruptedException e) { log.info(e.getMessage()); }
