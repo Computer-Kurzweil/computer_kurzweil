aa2872657d90985666868fbd3ac79b68001770aa
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-21 13:34:32 +0200

    Fixed #33, Fixed #34

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ function runGithubCi(){
 
 function makeRelease() {
     ./mvnw -B -DskipTests clean dependency:list install site site:deploy --file pom.xml
-    ./mvnw -B -DskipTests release:prepare
-    ./mvnw -B -DskipTests release:perform
+    #./mvnw -B -DskipTests release:prepare
+    #./mvnw -B -DskipTests release:perform
 }
 
 function run() {
