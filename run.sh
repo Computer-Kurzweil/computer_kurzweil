61a3e379802bf316f406ef40d3c21dedc01d297f
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-21 20:00:54 +0200

    work in progress

--- run.sh
+++ run.sh
@@ -10,9 +10,9 @@ function runGithubCi(){
 }
 
 function makeRelease() {
-    ./mvnw -B -DskipTests clean dependency:list install site site:deploy --file pom.xml
-    #./mvnw -DskipTests -Dresume=false -DdryRun=true release:prepare
-    #./mvnw -DskipTests -Dresume=false release:prepare release:perform #-Psign-artifacts-with-ogc
+    #./mvnw -B -DskipTests clean dependency:list install site site:deploy --file pom.xml
+    ./mvnw -DskipTests -Dresume=false -DdryRun=true release:prepare
+    ./mvnw -DskipTests -Dresume=false release:prepare release:perform #-Psign-artifacts-with-ogc
 }
 
 function publishSite() {
